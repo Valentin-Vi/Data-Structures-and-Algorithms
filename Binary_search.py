21ded6a1207779cd1c35e56b920b145320ddf0d8
Valentín <valentin.villa@protonmail.com>
2024-02-20 12:00:47 -0300

    Simple binary search algorithm example.

---
+++ Binary_search.py
@@ -0,0 +1,15 @@
+list = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+target = 8
+
+h = list[len(list) - 1]
+l = list[0]
+while True:
+    m = l + (h - l)//2
+    if list[m] > target:
+        h = m
+    elif list[m] < target:
+        l = m
+    else:
+        print(list[m])
+        break
