f95694980d2f7e71bcd075295f620d24fd2d1498
Valentin <153557240+Valentin-Vi@users.noreply.github.com>
2024-02-21 14:10:18 +0000

    Update BinarySearch.js

--- JavaScript/BinarySearch.js
+++ JavaScript/BinarySearch.js
@@ -3,10 +3,10 @@ for (let i=0; i < 100; i++) {
     list.push(i)
 }
 
-let target = Math.floor(Math.random() * (99 - 0 + 1) + 0);
-let h = list.length - 1;
-let m = Math.floor((list.length - 1) / 2);
 let l = 0;
+let h = list.length - 1;
+let m = l + Math.floor((h - l) / 2);
+let target = Math.floor(Math.random() * (99 - 0 + 1) + 0);
 
 while (true) {
     m = l + Math.floor((h - l) / 2);
